557295922b5f1fcd1855d03c6afee1d522675110
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-09-22 18:57:42 -0400

    Initial Commit

---
+++ AutoLayoutDSL/AutoLayoutDSL.h
@@ -0,0 +1,18 @@
+//
+//  AutoLayoutDSL.h
+//  AutoLayoutDSL
+//
+//  Created by Gustavo Anjos on 22/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoLayoutDSL.
+FOUNDATION_EXPORT double AutoLayoutDSLVersionNumber;
+
+//! Project version string for AutoLayoutDSL.
+FOUNDATION_EXPORT const unsigned char AutoLayoutDSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayoutDSL/PublicHeader.h>
+
+

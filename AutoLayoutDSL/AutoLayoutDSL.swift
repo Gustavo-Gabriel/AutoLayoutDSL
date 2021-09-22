ceed71ace43873c83b24a3451eaf499dbce9a8b8
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-09-22 19:10:19 -0400

    add tests

---
+++ AutoLayoutDSL/AutoLayoutDSL.swift
@@ -0,0 +1,12 @@
+public final class AutoLayoutDSL {
+    
+    let name = "AutoLayoutDSL"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a+b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a-b
+    }
+}
